f1635a4505bbb395641af87ba249e1b94be54aec
Youngmin Park <codepark2019@gmail.com>
2020-04-20 02:25:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
   sentence_split = sentence.split(" ")
-  reverse_each_word = sentence_split.each{|word|word.reverse}
+  reverse_each_word = sentence_split.each{|word|word.reverse!}
   return reverse_each_word.join(" ")
 end
