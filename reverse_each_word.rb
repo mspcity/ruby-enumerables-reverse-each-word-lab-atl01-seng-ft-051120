b66462d804fe2e7f54038888c7d3c625413dd941
Youngmin Park <codepark2019@gmail.com>
2020-04-20 02:52:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(sentence)
+  sentence.split.each{|word|word.reverse!}.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word|word.reverse}.join(" ")
+end
